c380f1f96db90ef49de159f79d2be1974df70937
FatimaCapilla <fatima.capilla.sancho@gmail.com>
2024-02-09 11:52:56 +0100

    change branche

---
+++ src/pages/About.jsx
@@ -0,0 +1,15 @@
+import Footer from '../components/Footer';
+import Body from '../components/BodyAbout';
+import Nav from '../components/Nav';
+
+const About = () => {
+  return (
+    <>
+        <Nav />
+        <BodyAbout />
+        <Footer />
+    </>
+  )
+}
+
+export default About
