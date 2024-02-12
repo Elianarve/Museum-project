b9da03ec5b5b4b9900d2d3964613809e9d8f526e
Elianarve <elirve2@outlook.es>
2024-02-12 12:37:37 +0100

    changes-footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,29 +7,55 @@ import lg from '../assets/logo-claro.png';
 
 
 const FooterContainer = styled.footer `
-.container {
+.container-footer {
     background-color: #13281B;
     color: white;
-    width: 100%;
     }
 
 .logo-m{
-  width: 8%;
-}    
+  width: 6%;
+}   
+
+.box{
+  display: flex;
+  justify-content: space-around;
+  padding-top: 1%;
+}
+
+.box-one{
+  margin-top: 2%;
+}
+
+.title-r {
+  font-size: 19px;
+}
+
+.icons{
+  display: flex;
+  justify-content: center;
+  gap: 6%;
+}
+
+.copy{
+  text-align: center;
+  margin-bottom: 0;
+  padding: 2%
+}
 
 @media only screen and (min-width: 320px) and (max-width: 1000px){
 
-.container {
+.container-footer {
 padding: 10px;
 text-align: center; 
 }
 
-.logo{
+.logo-m{
     margin: 10px;
-}
+    width: 35%;
 }
 
-.title{
+
+.title-r{
   font-size: 12px;
 }
 `;
@@ -37,20 +63,27 @@ text-align: center;
 const Footer = () => {
   return (
     <FooterContainer>
-        <div className='container'>
+        <div className='container-footer'>
+          <div className='box'>
+          <div className='box-one'>
          <img src={lg} alt="logo" className='logo-m' /> 
-         <p className='title'>REACT<br /> MUSEUM</p>
+         <p className='title-r'>REACT MUSEUM</p>
+         </div>
+         <div className='box2'>
          <p>Contacto</p>
          <p>676548934 - 904337658</p> 
          <p>contacto@reactmuseum.com</p>   
-         <p>C/ de Amaniel, 2, Centro, 28015 Madrid</p>    
+         <p>C/ de Amaniel, 2, Centro, 28015 Madrid</p> 
+         </div> 
+         </div> 
+         <hr /> 
          <div className='icons'>
             <img className='logo' src={insta} alt="instagram" />
             <img className='logo' src={fcb} alt="facebook" />
             <img className='logo' src={twiter} alt="twitter" />
             <img className='logo' src={youtube} alt="youtube" />
         </div> 
-        <p>Copyrigth &copy; 2024 REACT Museum</p>
+        <p className='copy'>Copyrigth &copy; 2024 REACT Museum</p>
          </div>
     </FooterContainer>
   )
