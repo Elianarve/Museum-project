a7f2dbf9d1cd4b9c7a852dea71f1315de862939a
= <=>
2024-02-20 12:47:00 +0100

    change form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,25 +2,31 @@ import { useState, useEffect } from 'react';
 import styled from 'styled-components';
 
 const FormContainer = styled.div`
-   .formulario {
-     display: flex;
-  //   flex-direction: row;
-   } 
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-left: 30px; /* Ajusta el margen a la izquierda */
 
-  .inputs {
-    width: 35vw; 
+  @media only screen and (min-width: 768px) {
+    flex-direction: row;
+    justify-content: space-between;
   }
+`;
 
-.box{ 
-    width: 37%;
-    margin: 2% 0 3% 5%;
-    padding: 10px;
-    border: 1px solid #ccc;
-    border-radius: 15px;
-    background-color: #434D68;
-    font-family: 'Roboto',sans-serif;
-}
-`
+const Box = styled.div`
+  width: 100%;
+  max-width: 600px; /* Ajusta el ancho máximo del formulario */
+  margin-bottom: 20px;
+  padding: 10px;
+  border: 1px solid #ccc;
+  border-radius: 15px;
+  background-color: #434D68;
+  font-family: 'Roboto', sans-serif;
+
+  @media only screen and (min-width: 768px) {
+    width: 65%; /* Ajusta el ancho del formulario */
+  }
+`;
 
 const Heading = styled.h2`
   font-size: 1.5rem;
@@ -67,23 +73,16 @@ const Button = styled.button`
 `;
 
 const MapContainer = styled.div`
-  margin-top: 4%;
   width: 100%;
-  display: flex;
-  justify-content: center;
-}
-
-@media only screen and (min-width: 320px) and (max-width: 1000px){
-  .formulario {
-    display: block;   
+  max-width: 400px; /* Ajusta el ancho máximo del mapa */
+  margin-top: 4%;
+  margin´right: 30px;
+  @media only screen and (min-width: 768px) {
+    width: 30%; /* Ajusta el ancho del mapa */
+    margin-top: 0;
   }
+`;
 
-  .box{ 
-    width: 100%;
-}
-
-}
-`
 const ContactForm = () => {
   const [formData, setFormData] = useState({    
     nombre: '',
@@ -111,7 +110,6 @@ const ContactForm = () => {
 
   const Messages = () => {
     const [data, setData] = useState(null);
-           //donde se guardan, lo que hace que cambie = lo que inicia
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
@@ -156,70 +154,68 @@ const ContactForm = () => {
 
   return (
     <FormContainer className='contenedor'>
-      <div className='formulario'>
-        <div className='box'>
-          <Heading>CONTÁCTANOS</Heading>
-          <form onSubmit={handleSubmit}>
-            <InputGroup className='inputs'>
-              <Label htmlFor="name">Nombre:</Label>
-              <Input
-                type="text"
-                id="name"
-                name="nombre"
-                value={formData.nombre}
+      <Box>
+        <Heading>CONTÁCTANOS</Heading>
+        <form onSubmit={handleSubmit}>
+          <InputGroup>
+            <Label htmlFor="name">Nombre:</Label>
+            <Input
+              type="text"
+              id="name"
+              name="nombre"
+              value={formData.nombre}
+              onChange={handleChange}
+              required
+            />
+          </InputGroup>
+          <InputGroup>
+            <Label htmlFor="email">Email:</Label>
+            <Input
+              type="email"
+              id="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              required
+            />
+          </InputGroup>
+          <InputGroup>
+            <Label htmlFor="message">Mensaje:</Label>
+            <Input
+              as="textarea"
+              id="message"
+              name="mensaje"
+              value={formData.mensaje}
+              onChange={handleChange}
+              required
+            />
+          </InputGroup>
+          <InputGroup>
+            <CheckboxLabel>
+              <CheckboxInput
+                type="checkbox"
+                id="acceptCookies"
+                name="acceptCookies"
+                checked={formData.acceptCookies}
                 onChange={handleChange}
-                required
               />
-            </InputGroup>
-            <InputGroup className='inputs'>
-              <Label htmlFor="email">Email:</Label>
-              <Input
-                type="email"
-                id="email"
-                name="email"
-                value={formData.email}
-                onChange={handleChange}
-                required
-              />
-            </InputGroup>
-            <InputGroup className='inputs'>
-              <Label htmlFor="message">Mensaje:</Label>
-              <Input
-                as="textarea"
-                id="message"
-                name="mensaje"
-                value={formData.mensaje}
-                onChange={handleChange}
-                required
-              />
-            </InputGroup>
-            <InputGroup className='inputs'>
-              <CheckboxLabel>
-                <CheckboxInput
-                  type="checkbox"
-                  id="acceptCookies"
-                  name="acceptCookies"
-                  checked={formData.acceptCookies}
-                  onChange={handleChange}
-                />
-                Aceptar política de cookies
-              </CheckboxLabel>
-            </InputGroup>
-            <Button type="submit">Enviar Mensaje</Button>
-          </form>
-        </div>
-        <MapContainer>
-          <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3037.27183079143!2d-3.711619825264592!3d40.42497935513926!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd4228648c68edc1%3A0x6ffead3529527c69!2sC.%20de%20Amaniel%2C%202%2C%20Centro%2C%2028015%20Madrid!5e0!3m2!1ses!2ses!4v1707398554055!5m2!1ses!2ses"
-            width="80%"
-            height="400px"
-            style={{ border: 0 }}
-            allowFullScreen=""
-            loading="lazy"
-            referrerPolicy="no-referrer-when-downgrade"
-          ></iframe>
-        </MapContainer>
-      </div>
+              Aceptar política de cookies
+            </CheckboxLabel>
+          </InputGroup>
+          <Button type="submit">Enviar Mensaje</Button>
+        </form>
+      </Box>
+      <MapContainer>
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3037.27183079143!2d-3.711619825264592!3d40.42497935513926!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd4228648c68edc1%3A0x6ffead3529527c69!2sC.%20de%20Amaniel%2C%202%2C%20Centro%2C%2028015%20Madrid!5e0!3m2!1ses!2ses!4v1707398554055!5m2!1ses!2ses"
+          width="100%"
+          height="400px"
+          style={{ border: 0 }}
+          allowFullScreen=""
+          loading="lazy"
+          referrerPolicy="no-referrer-when-downgrade"
+        ></iframe>
+      </MapContainer>
       {/* Aquí incluye el componente Messages */}
       <Messages />
     </FormContainer>
