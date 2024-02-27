98cedae45da086cab11ed4eba0b86bc0af3a264d
Elianarve <elirve2@outlook.es>
2024-02-27 11:25:00 +0100

    test-links

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 import logo from '../assets/logo-oscuro.png';
-import Created from '../pages/CreatedAnimal';
+// import Created from '../pages/CreatedAnimal';
 
 const NavContainer = styled.nav`
 .header {
