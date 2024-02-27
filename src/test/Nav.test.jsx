be2629f6006fd59e5bbc752285e23e556775239b
Elianarve <elirve2@outlook.es>
2024-02-27 09:57:16 +0100

    testeo

--- src/test/Nav.test.jsx
+++ src/test/Nav.test.jsx
@@ -1,10 +1,20 @@
 import { render, screen } from '@testing-library/react';
 import Nav from '../components/Nav';
-import test from '../vite.config';
-import { expect } from 'vitest';
+import { expect, test, describe, beforeEach } from 'vitest';
+
+describe('App testing', () =>{
+    beforeEach(()=>{
+        render(<Nav/ >);
+    }); 
+    
 
 test('render Nav', () =>{
     render(<Nav />);
     const logoNav = screen.getByText(/REACT MUSEUM/i); 
     expect(logoNav).toBeDefined();
-})
+});
+
+
+
+
+});
